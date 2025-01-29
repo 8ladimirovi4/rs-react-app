871b26b4b4a15eee1291d77e6c2448abc02271fd
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-01-29 10:04:44 +0300

    fix: fix linting issues

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   plugins: [plugin()],
   resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-        'assets': path.resolve(__dirname, 'src/assets'),
-        'components': path.resolve(__dirname, 'src/components'),
-        'pages': path.resolve(__dirname, 'src/pages'),
-        'store': path.resolve(__dirname, 'src/store'),
-        'styles': path.resolve(__dirname, 'src/styles')
-    }
-},
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      store: path.resolve(__dirname, 'src/store'),
+      styles: path.resolve(__dirname, 'src/styles'),
+    },
+  },
 });
