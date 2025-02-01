67ddb0e515057438a4f4609a70db0ee712c92687
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-02-01 17:19:46 +0300

    feat: set up prettier and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       pages: path.resolve(__dirname, 'src/pages'),
       store: path.resolve(__dirname, 'src/store'),
       styles: path.resolve(__dirname, 'src/styles'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
     },
   },
 });
