46c5110308050689e4918845028285fa6588932c
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-02-01 17:20:39 +0300

    feat: add error boundary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
 import 'styles/output.css';
+import TopControls from 'layouts/Top_controls/TopControls';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 function App() {
-  return <h1>установил Tailwind</h1>;
+  return (
+    <ErrorBoundary>
+      <TopControls />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import { Component, ErrorInfo } from 'react';
+import { ErrorBoundaryProps } from './types';
+
+type ErrorBoundaryState = {
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+};
+
+export default class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({ error, errorInfo });
+    // Можно также отправить ошибку в сервис логирования
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
